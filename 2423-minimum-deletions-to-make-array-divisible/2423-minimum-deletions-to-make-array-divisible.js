71a3399b5c80824622339355290a828609c4efea
Muhammed Shadil VP <shadilx8@gmail.com>
2024-11-27 08:45:40 +0530

    Time: 38 ms (33.33%), Space: 70.4 MB (57.14%) - LeetHub

---
+++ 2423-minimum-deletions-to-make-array-divisible/2423-minimum-deletions-to-make-array-divisible.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} numsDivide
+ * @return {number}
+ */
+var minOperations = function(nums, numsDivide) {
+    let sort = nums.sort((a,b)=> a - b)
+    let set1 = new Set(sort)
+    console.log('sort',sort)
+    let set2 = new Set(numsDivide)
+    let newArr1 = [...set1]
+    let newArr2 = [...set2]
+
+    for(let i = 0 ; i < newArr1.length ; i ++){
+        let result = true
+        for(let j = 0; j < newArr2.length ; j ++){
+            if(newArr2[j] % newArr1[i] !== 0 ){
+                result = false
+                break ; 
+            }
+        }
+        if(result){
+            for(let k = 0 ; k < nums.length ; k ++){
+                if(newArr1[i] === nums[k]){
+                    return k
+                }
+            }
+        }
+    }
+    return -1
+};
