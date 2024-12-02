ddf4d713c9e1df6b33765163bba13c5f1ec193b1
Muhammed Shadil VP <shadilx8@gmail.com>
2024-12-02 10:27:59 +0530

    Time: 63 ms (5.28%), Space: 48.9 MB (59.15%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map(fn)
+    let res = []
+    for (let i = 0; i<arr.length; i++){
+       res.push(fn(arr[i],i))
+    }
+    return res
 };
