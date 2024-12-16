388466deca7dd7b46e02b0f2c6fce42d350c9feb
Muhammed Shadil VP <shadilx8@gmail.com>
2024-12-16 10:08:32 +0530

    Time: 3 ms (20.57%), Space: 53.3 MB (33.94%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.js
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.js
@@ -7,18 +7,21 @@
 var countMatches = function(items, ruleKey, ruleValue) {
     let count = 0
     for(let i = 0; i<items.length; i++ ){
-        if(ruleKey === "type"){
-            if(items[i][0] === ruleValue){
-                count ++
-            }
-        }else if(ruleKey === "color"){
-            if(items[i][1] === ruleValue){
-                count ++
-            }
-        }else{
-            if(items[i][2] === ruleValue){
-                count ++
-            }
+        switch(ruleKey){
+            case "type":
+                if(items[i][0] === ruleValue){
+                    count ++
+                }
+            break ;
+            case "color":
+                if(items[i][1] === ruleValue){
+                    count ++
+                }
+            break;
+            default : 
+                if(items[i][2] === ruleValue){
+                    count ++
+                }  
         }
     }
     return count   
