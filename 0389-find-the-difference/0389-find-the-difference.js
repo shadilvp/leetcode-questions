e0af3f834eb547aceb39e153d06c506c23ad0256
Muhammed Shadil VP <shadilx8@gmail.com>
2024-12-17 10:00:32 +0530

    Time: 9 ms (28.96%), Space: 50.1 MB (86.18%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,14 +4,11 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let a = s.split("").sort().join("")
-    let b = t.split("").sort().join("")
-    console.log(a)
-    console.log(b)
+    let a = s.split("").sort()
+    let b = t.split("").sort()
     for(let i=0;i<b.length;i++){
         if(a[i] != b[i]){
             return b[i]
         }
     }
-    return ""
 };
