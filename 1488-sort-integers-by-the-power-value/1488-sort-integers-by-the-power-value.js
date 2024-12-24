f057c26911428a42d9ee01b4105003580ba6c82e
Muhammed Shadil VP <shadilx8@gmail.com>
2024-12-24 09:52:54 +0530

    Time: 40 ms (31.94%), Space: 56.8 MB (5.56%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.js
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.js
@@ -6,20 +6,25 @@
  */
 var getKth = function(lo, hi, k) {
     let expandedArr = []
+    //for expanding the array lo to hi
     for(let i= lo ; i <= hi ; i++ ){
         expandedArr.push(i)
     }
-    const powFun = (num) => {
+    //we don't have an exponent so we are making the power function 
+    const powFun = (num) => { 
         let steps = 0;
         for(let current = num ; current != 1; steps ++){
             current = current % 2 == 0 ? current / 2 : 3*current + 1 
         }
         return steps
     }
+    //making the powered array using the powerfunction
     let poweredArr = expandedArr.map(powFun)
+    //sorting the original array based on the powered array values
     const sortedOrgArr = expandedArr
         .map((num, index) => ({ num, power: poweredArr[index] }))
         .sort((a,b)=>a.power -b.power)  
         .map(obj=>obj.num)
+    //returning the k from the sorted original array
     return sortedOrgArr[k-1]
 };
