c4b69f9c09e6cfdfaa2164c2398797100a25ba55
Muhammed Shadil VP <shadilx8@gmail.com>
2025-01-20 09:49:06 +0530

    Time: 0 ms (100%), Space: 48.9 MB (59.78%) - LeetHub

--- 2365-percentage-of-letter-in-string/2365-percentage-of-letter-in-string.js
+++ 2365-percentage-of-letter-in-string/2365-percentage-of-letter-in-string.js
@@ -7,7 +7,6 @@ var percentageLetter = function(s, letter) {
     let strLength = s.length;
     let toArr = s.split('');
     let letterLength = toArr.filter((s)=>s == letter).length;
-    let result =  letterLength/strLength * 100;
-    return Math.floor(result)
+    return Math.floor(letterLength/strLength * 100);
 
 };
