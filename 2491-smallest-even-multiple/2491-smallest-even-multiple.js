33239183534f893f22540061bad5c4677d509bc2
Muhammed Shadil VP <shadilx8@gmail.com>
2025-03-13 09:47:25 +0530

    Time: 5 ms (0.32%), Space: 54 MB (10.26%) - LeetHub

---
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+
+    for(let i = 1 ; i <= n+3 ; i ++){
+        let a =  i * n ;
+        console.log(a)
+        if(a % 2 == 0){
+            return a
+        }
+    }
+};
