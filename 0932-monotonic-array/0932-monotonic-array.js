496fb2083cf7806954590767c552809ca67e7aac
Muhammed Shadil VP <shadilx8@gmail.com>
2025-04-12 09:32:55 +0530

    Time: 3 ms (52.65%), Space: 67.1 MB (45.96%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    // if(nums[nums.length - 1] <= 0){
+    //     nums.reverse()
+    // }
+    if(nums[nums.length - 1] < nums[0]){
+        nums.reverse()
+    }
+
+    for(let i = 0 ; i <= nums.length-1 ; i++){
+        if(nums[i] > nums[i+1]){
+            return false
+        }
+       
+    }
+     return true
+};
