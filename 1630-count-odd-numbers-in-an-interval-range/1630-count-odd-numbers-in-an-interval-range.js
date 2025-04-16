9435bdeb9cd88538e476574beef90f2976fd468d
Muhammed Shadil VP <shadilx8@gmail.com>
2025-04-16 10:35:45 +0530

    Time: 2711 ms (34.84%), Space: 55.6 MB (7.79%) - LeetHub

---
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    let count = 0
+    for(let i = low ; i <= high ; i++){
+        if(i % 2 !== 0){
+            count++
+        }
+        
+    }
+    console.log(count)
+    return count;
+};
