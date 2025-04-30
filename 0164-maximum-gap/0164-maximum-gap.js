b70f9baeb8e5e91118f49346e3cf570d50b08419
Muhammed Shadil VP <shadilx8@gmail.com>
2025-04-30 09:56:38 +0530

    Time: 149 ms (68.51%), Space: 67.7 MB (94.81%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function (nums) {
+    if (nums.length < 2) return 0;
+
+    nums.sort((a, b) => a - b);
+
+    let maxGap = 0;
+    for (let i = 1; i < nums.length; i++) {
+        maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
+    }
+
+    return maxGap;
+
+};
