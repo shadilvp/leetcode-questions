2fed1802dc9f2e8c63b4efdb0e43f7eae29bad88
Muhammed Shadil VP <shadilx8@gmail.com>
2025-05-03 10:23:33 +0530

    Time: 0 ms (100%), Space: 53.1 MB (83.91%) - LeetHub

--- 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.js
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.js
@@ -5,7 +5,6 @@
 var isSameAfterReversals = function(num) {
     let reversed1 = Math.abs(num.toString().split('').reverse().join(''))
     let reversed2 = Math.abs(reversed1.toString().split('').reverse().join(''))
-    console.log(reversed2)
     if(reversed2 === num){
         return true
     }
